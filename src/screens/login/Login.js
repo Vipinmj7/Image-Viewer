46274b2648178c955abd30656c7ba10ceb49e909
vipinmj7 <vipinmj7@gmail.com>
2021-06-25 10:36:22 +0530

    Added remaining Login functionalities and form helper content

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Header from "../../common/header/Header";
+import './Login.css';
 import {
   Button,
   Card,
@@ -29,12 +30,42 @@ export default class Login extends Component {
     this.state = {
       username: "",
       password: "",
+      reqpassword: "dispNone",
+      requsername: "dispNone",
+      incorrectUsernamePassword: "dispNone"
     };
   }
+  inputUsernameChangeHandler = (e) => {
+    this.setState({ username: e.target.value });
+  };
 
+  inputPasswordChangeHandler = (e) => {
+    this.setState({ password: e.target.value });
+  };
   loginHoverHandler = (e) => {
     e.target.style.cursor = "pointer";
   };
+  loginClickHandler = () => {
+
+    this.setState({ incorrectUsernamePassword: "dispNone" });
+
+    this.state.username.trim() === ""? this.setState({ requsername: "dispBlock" }): this.setState({ requsername: "dispNone" });
+    this.state.password.trim() === ""? this.setState({ reqpassword: "dispBlock" }): this.setState({ reqpassword: "dispNone" });
+
+    if (
+      this.state.username.trim() === "admin" &&
+      this.state.password.trim() === "password"
+    ) {
+      sessionStorage.setItem(
+        "access-token",
+        "IGQVJXOFdRVTJnQU03bHlCaTFZASUItVzl6WHRPMnJhWlF2RlR1dzRNaHZALV3RpdHp4U3RaZAGhYSDdaNDlILW1xUmljRVdwdmR1bVM2TmgzckxFcnVvdWl3QVBoOXRuWlMyRlhvSGdQZAndDSVFSOVpiUzdheWs4YUdRaW9J"
+      );
+      this.setState({ incorrectUsernamePassword: "dispNone" });
+    } else {
+      this.setState({ incorrectUsernamePassword: "dispBlock" });
+    }
+  };
+
   render() {
     return (
       <div>
@@ -45,7 +76,15 @@ export default class Login extends Component {
             <br />
             <FormControl required style={{ width: "100%" }}>
               <InputLabel htmlFor="username"> Username </InputLabel>
-              <Input id="username" type="text" username={this.state.username} />
+              <Input
+                id="username"
+                type="text"
+                username={this.state.username}
+                onChange={this.inputUsernameChangeHandler}
+              />
+              <FormHelperText className={this.state.requsername}>
+                <span className="red">required</span>
+              </FormHelperText>
             </FormControl>
             <br />
             <br />
@@ -55,14 +94,23 @@ export default class Login extends Component {
                 id="password"
                 type="password"
                 password={this.state.password}
+                onChange={this.inputPasswordChangeHandler}
               />
+              <FormHelperText className={this.state.reqpassword}>
+                <span className="red">required</span>
+              </FormHelperText>
             </FormControl>
             <br />
+            <br />
+            <div className={this.state.incorrectUsernamePassword}>
+              <span className="red"> Incorrect username and/or password </span>
+            </div>
             <br />
             <Button
               variant="contained"
               color="primary"
               onMouseOver={this.loginHoverHandler}
+              onClick={this.loginClickHandler}
             >
               LOGIN
             </Button>
